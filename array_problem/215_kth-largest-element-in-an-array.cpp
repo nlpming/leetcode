e5186fd75b95f86d7d7ac7289e97c3f0614a5651
nlpming <qqchenzhiming@163.com>
2021-08-22 00:29:32 +0800

    add array_problem

--- array_problem/215_kth-largest-element-in-an-array.cpp
+++ array_problem/215_kth-largest-element-in-an-array.cpp
@@ -2,14 +2,46 @@
 // Created by 陈志明 on 2021/8/21.
 //
 
+/*
+ * 考点：快速排序；
+ * 难度：***
+ * */
 class Solution {
 public:
-    int helper(){
 
+    //一趟快速排序：注意是将大的元素，放在pivot的左边；
+    int partition(vector<int>& nums, int left, int right){
+
+        int pivot = nums[left]; //NOTE: 从大到小排序；
+        while(left < right){
+            while(left < right && nums[right] <= pivot)
+                right--;
+            nums[left] = nums[right];
+
+            while(left < right && nums[left] >= pivot)
+                left++;
+            nums[right] = nums[left];
+        }
+
+        nums[left] = pivot;
+        return left;
+    }
+
+    int helper(vector<int>& nums, int left, int right, int k){
+        int pivot_idx = partition(nums, left, right);
+        if(pivot_idx + 1 == k){ //NOTE: 找到第k大的元素；
+            return nums[pivot_idx];
+        }
+
+        if(pivot_idx < k){ //NOTE: 第k大的元素，在右边；
+            return helper(nums, pivot_idx+1, right, k);
+        }else{
+            return helper(nums, left, pivot_idx-1, k);
+        }
     }
 
     int findKthLargest(vector<int>& nums, int k) {
 
-
+        return helper(nums, 0, nums.size()-1, k);
     }
 };
