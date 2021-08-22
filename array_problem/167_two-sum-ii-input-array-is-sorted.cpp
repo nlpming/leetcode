9beeefe0da184964f45f2b9d6baee3223bcc23c2
nlpming <qqchenzhiming@163.com>
2021-08-22 09:33:04 +0800

    add array_problem/167_two-sum-ii-input-array-is-sorted.cpp

---
+++ array_problem/167_two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 陈志明 on 2021/8/22.
+//
+
+/*
+ * 考点：对撞指针；
+ * 难度：*
+ * */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size()-1;
+        vector<int> res;
+
+        //注意：
+        //1.left指针初始化为0，right指针初始化为nums.size()-1;
+        //2.left, right相向移动，如果nums[left] + nums[right] == target则返回（left+1, right+1）；
+
+        while(left < right){
+            if(numbers[left] + numbers[right] == target){
+                res.push_back(left+1);
+                res.push_back(right+1);
+                return res;
+            }else if(numbers[left] + numbers[right] > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+
+        return res;
+    }
+};
+
