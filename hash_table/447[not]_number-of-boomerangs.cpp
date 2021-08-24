740f7775fa1758e0adf91eb54b47e69cb4cc7ddb
nlpming <qqchenzhiming@163.com>
2021-08-24 11:55:06 +0800

    update hash_table/447[not]_number-of-boomerangs.cpp

--- hash_table/447[not]_number-of-boomerangs.cpp
+++ hash_table/447[not]_number-of-boomerangs.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    int getDistance(vector<int> p1, vector<int> p2){
+    long getDistance(vector<int> p1, vector<int> p2){
         //NOTE: 此处不能使用pow函数，返回的类型为double；
         return (p1[0] - p2[0])*(p1[0] - p2[0]) + (p1[1] - p2[1])*(p1[1] - p2[1]);
     }
@@ -14,7 +14,7 @@ public:
 
         for(int i = 0; i < points.size(); i++){
             // 1. 找到和points[i]这个点，距离相同的点的个数；
-            unordered_map<int, int> record; //NOTE: key为距离，value为距离相同的点的个数；
+            unordered_map<long, int> record; //NOTE: key为距离，value为距离相同的点的个数；
             for(int j = 0; j < points.size(); j++){
                 if(j == i) continue;
                 record[getDistance(points[i], points[j])]++;
