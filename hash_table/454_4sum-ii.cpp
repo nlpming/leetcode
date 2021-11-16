a263d5f9489eecb7972b75272c52c49fded4cfa7
nlpming <qqchenzhiming@163.com>
2021-11-16 12:21:05 +0800

    update hash_table/pics/WX20211116-122008@2x.png

--- hash_table/454_4sum-ii.cpp
+++ hash_table/454_4sum-ii.cpp
@@ -13,7 +13,8 @@ public:
         unordered_map<int, int> record;
         int res = 0;
 
-        //NOTE: 将A[i] + B[j]之和放入record中，并记录总共有多少种可能组合；【灵活选择key - value】
+        //NOTE: 将A[i] + B[j]之和放入record中，并记录总共有多少种可能组合；
+        // key -> 两数之和，value -> 和相同的数量；
         for(int i = 0; i < nums1.size(); i++){
             for(int j = 0; j < nums2.size(); j++){
                 record[nums1[i] + nums2[j]]++;
@@ -23,7 +24,7 @@ public:
         //NOTE: 结果可以存在重复；
         for(int i = 0; i < nums3.size(); i++){
             for(int j = 0; j < nums4.size(); j++){
-                int target = 0 - (nums3[i] + nums4[j]);
+                int target = -(nums3[i] + nums4[j]);
                 if(record.find(target) != record.end()){
                     res += record[target];
                 }
